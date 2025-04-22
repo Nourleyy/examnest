4ae970eb947dae90b5318c234f02965a2f97771b
Mahmoud Nasr <239.nasr@gmail.com>
2025-04-22 23:00:38 +0200

    Refactor project structure and add TestController
    Removed unused folder reference in ExamNest.csproj.
    Added TestController with API endpoint to retrieve student exam results using AppDBContext.

---
+++ Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using ExamNest.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExamNest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        private readonly AppDBContext appDBContext;
+        public TestController(AppDBContext _app)
+        {
+
+            appDBContext = _app;
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            // Example of using the DbContext
+            var exam_results = await appDBContext.GetProcedures().GetStudentExamResultsAsync(1, 1);
+            return Ok(exam_results);
+        }
+
+    }
+}
